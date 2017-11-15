94f09013cdbd464b90ca4ab9c216168e20829521
Harish <harish@Harishs-Mac-mini.local>
2017-11-15 16:04:32 +0530

    upload

---
+++ KavyaFramework/KavyaFramework/Kavya.swift
@@ -0,0 +1,15 @@
+//
+//  Kavya.swift
+//  KavyaFramework
+//
+//  Created by Harish on 15/11/17.
+//  Copyright © 2017 Harish. All rights reserved.
+//
+
+import UIKit
+
+public class Kavya: NSObject {
+    public func kavya () {
+        print("kavya")
+    }
+}
---
+++ KavyaFramework/KavyaFramework/KavyaFramework.h
@@ -0,0 +1,19 @@
+//
+//  KavyaFramework.h
+//  KavyaFramework
+//
+//  Created by Harish on 15/11/17.
+//  Copyright © 2017 Harish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KavyaFramework.
+FOUNDATION_EXPORT double KavyaFrameworkVersionNumber;
+
+//! Project version string for KavyaFramework.
+FOUNDATION_EXPORT const unsigned char KavyaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KavyaFramework/PublicHeader.h>
+
+
